996573e8ac0905b1080d490ca5e54a15fa4c4536
Mithun Gore <mithungore@gmail.com>
2020-09-15 11:57:34 +0530

    Add InfoMetricFamily metric for firmware version collector.
    Added InfoMetricFamily to get firmware version
    
    Signed-off-by: Mithun Gore <mithungore@gmail.com>

--- collectors/UcsmCollector.py
+++ collectors/UcsmCollector.py
@@ -1,4 +1,4 @@
-from prometheus_client.core import GaugeMetricFamily
+from prometheus_client.core import InfoMetricFamily
 from BaseCollector import BaseCollector
 
 
@@ -7,18 +7,17 @@ class UcsmCollector(BaseCollector):
         super().__init__(creds, config)
 
     def describe(self):
-        yield GaugeMetricFamily("ucsm_metrics", "ucsm_collector_registered")
+        yield InfoMetricFamily("ucsm_metrics", "ucsm_collector_registered")
 
     def collect(self):
         print("UcsmCollector: Get updated handles !")
         self.get_handles()
-        g = GaugeMetricFamily('ucsm_info', 'UCSM server information',
-                              labels=['server', 'firmware_version'])
+        g = InfoMetricFamily('ucsm_info', 'UCSM server information')
         for server, handle in self.handles.items():
             sys = handle.query_dn("sys")
             firmware_status = handle.query_children(sys, class_id="FirmwareStatus")
             firmware_version = firmware_status[0].package_version
-            g.add_metric(labels=[server, firmware_version], value=0)
+            g.add_metric(labels=["server", "firmware_version"], value={"server":server, "firmware_version":firmware_version})
         yield g
 
         self.logout_handles()
