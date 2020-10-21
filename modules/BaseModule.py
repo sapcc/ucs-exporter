71c6da442e91568528d32d8bead754e35b0b5336
Daniel Poelzleithner <daniel.poelzleithner@sap.com>
2020-10-21 18:41:00 +0200

    Replace unsopported password characters

--- modules/BaseModule.py
+++ modules/BaseModule.py
@@ -1,5 +1,17 @@
-import master_password as master_pass
+import re
 
+import master_password as master_pass
+from ucsmsdk.mometa.aaa.AaaUser import AaaUser
+
+def fix_ucsm_password( pwd, repl='@' ):
+    re_pattern = AaaUser.prop_meta['pwd'].restriction.pattern
+    unsupported_chars = re.sub( re_pattern, '', pwd )
+    if 0 == len(unsupported_chars):
+        return pwd
+    esc_unsupported_re = '[' + re.escape( unsupported_chars ) + ']'
+    safe_pass = re.sub( esc_unsupported_re, repl, pwd )
+    # echo("unsupported_chars: %s -> %s"%(unsupported_chars, safe_pass))
+    return safe_pass
 
 class BaseClass(object):
     def __init__(self, server, user, master_pasword):
@@ -13,4 +25,4 @@ class BaseClass(object):
         :return:
         """
         mpw = master_pass.MPW(self.user, self.master_password)
-        return mpw.password(self.ucs_server)
+        return fix_ucsm_password(mpw.password(self.ucs_server))
