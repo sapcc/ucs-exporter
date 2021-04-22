e498b10db74d292f6881a3e07bfdb3653d4ac0da
d032408 <bernd.kuespert@sap.com>
2021-04-22 10:20:52 +0200

    Constantly report a metric and not only if there is a fault. Add the chassis-rn as label.

--- collectors/UcsmChassisFaultCollector.py
+++ collectors/UcsmChassisFaultCollector.py
@@ -10,7 +10,7 @@ class UcsmChassisFaultCollector(BaseCollector):
         return {
             "faults": GaugeMetricFamily("ucsm_chassis_faults",
                                         "UCSM server chassis faults",
-                                        labels=['server', 'fault',
+                                        labels=['server', 'chassi', 'fault',
                                                 'severity', 'description'])
         }
 
@@ -19,11 +19,15 @@ class UcsmChassisFaultCollector(BaseCollector):
 
         sys = handle.query_dn("sys")
         chassis = self.query(handle.query_children, sys, class_id="EquipmentChassis")
+        
+        logger.info("{0}: Received {1} chassis from query".format(server, len(chassis)))
         for chassi in chassis:
             faults = self.query(handle.query_children, chassi, class_id="FaultInst")
-            logger.info("Faults ..... {0} - {1}".format(faults, server))
             if faults:
+                logger.info("{0}: {1} Faults detected: {2}".format(server, chassi.rn, faults))
                 for fault in faults:
-                    g.add_metric(labels=[server, fault.cause, fault.severity, fault.descr],
-                                    value=0)
+                    g.add_metric(labels=[server, fault.cause, chassi.rn, fault.severity, fault.descr], value=1)
+            else:
+                logger.info("{0}: {1} No Faults detected.".format(server, chassi.rn))
+                g.add_metric(labels=[server, chassi.rn], value=0)
         yield g
