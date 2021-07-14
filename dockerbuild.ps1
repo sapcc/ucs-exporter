e4a39d3777276746170ad91b2fcef0bcc65f4a48
d032408 <bernd.kuespert@sap.com>
2021-07-14 21:02:51 +0200

    PS script for building docker image locally

---
+++ dockerbuild.ps1
@@ -0,0 +1,6 @@
+$version = get-date -Format yyyyMMddHHmmss
+$image = "ucs-exporter"
+
+docker login keppel.eu-de-1.cloud.sap
+docker build . -t keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
+docker push keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
